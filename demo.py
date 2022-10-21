336747919e99abd5b6476fb18297f48c2c342592
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-21 17:06:17 +0530

    game code 1

---
+++ demo.py
@@ -0,0 +1,13 @@
+#wap to find hyptoneuse 
+
+# function defination
+def pythagoras():
+    per = int(input("Enter the perpendicular of the triangle: "))
+    base = int(input("Enter the base of the triangle: "))
+    hyp = (per**2 + base**2)**0.5
+    print(hyp)
+
+
+# function call
+pythagoras()
+pythagoras()
