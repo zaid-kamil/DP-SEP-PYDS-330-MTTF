2d8d915d2e45dbe96e6a3aa68b1947ff1d727535
zaidkamil <xaidmetamorphos@gmail.com>
2022-10-28 16:47:58 +0530

    movement

--- game1.py
+++ game1.py
@@ -1,9 +1,13 @@
 import pgzrun
+
 HEIGHT = 500
 WIDTH = 800
+
 def draw():
     # fill the screen with a color 
     screen.fill("blue")
     # display the image on the screen at (100, 100) coordinates
     screen.blit('ironman', (100, 100))
+
+    
 pgzrun.go()
